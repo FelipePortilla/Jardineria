526c8c370abca2ede33c529b55b714acf462eb00
FelipePortilla <128866686+FelipePortilla@users.noreply.github.com>
2023-11-21 11:25:54 -0500

    feat: :sparkles: New funtions in UnitOfwork

--- Backend/JarApi/Controllers/ClienteController.cs
+++ Backend/JarApi/Controllers/ClienteController.cs
@@ -11,36 +11,36 @@ using ApiApolo.Controllers;
 
 namespace JarApi.Controllers;
 
-    public class ClienteController : BaseController
+public class ClienteController : BaseController
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public ClienteController(IUnitOfWork unitOfWork, IMapper mapper)
     {
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IMapper _mapper;
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
 
-        public ClienteController(IUnitOfWork unitOfWork, IMapper mapper)
-        {
-            _unitOfWork = unitOfWork;
-            _mapper = mapper;
-        }
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<IEnumerable<ClienteDto>>> Get()
+    {
+        var clientes = await _unitOfWork.Clientes.GetAllAsync();
 
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<IEnumerable<ClienteDto>>> Get()
-        {
-            var clientes = await _unitOfWork.Clientes.GetAllAsync();
+        return _mapper.Map<List<ClienteDto>>(clientes);
+    }
 
-            return _mapper.Map<List<ClienteDto>>(clientes);
-        }
-
-        [HttpGet("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<ClienteDto>> Get(int id)
-        {
-            var cliente = await _unitOfWork.Clientes.GetByIdInt(id);
-            if (cliente == null)
-                return NotFound();
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<ClienteDto>> Get(int id)
+    {
+        var cliente = await _unitOfWork.Clientes.GetByIdInt(id);
+        if (cliente == null)
+            return NotFound();
 
         return _mapper.Map<ClienteDto>(cliente);
     }
@@ -59,36 +59,36 @@ namespace JarApi.Controllers;
         clienteDto.CodigoCliente = cliente.CodigoCliente;
         return CreatedAtAction(nameof(Post), new { id = clienteDto.CodigoCliente }, clienteDto);
     }
-[HttpPut("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<ClienteDto>> Put(int id, [FromBody] ClienteDto clienteDto)
-        {
-            if (clienteDto == null)
-                return NotFound();
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<ClienteDto>> Put(int id, [FromBody] ClienteDto clienteDto)
+    {
+        if (clienteDto == null)
+            return NotFound();
 
-            var cliente = await _unitOfWork.Clientes.GetByIdInt(id);
-            if (cliente == null)
-                return NotFound();
+        var cliente = await _unitOfWork.Clientes.GetByIdInt(id);
+        if (cliente == null)
+            return NotFound();
 
-            var cliente1 = _mapper.Map<Cliente>(clienteDto);
-            _unitOfWork.Clientes.Update(cliente1);
-            await _unitOfWork.SaveAsync();
-            return clienteDto;
-        }
-        [HttpDelete("{id}")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var cliente = await _unitOfWork.Clientes.GetByIdInt(id);
-            if (cliente == null)
-                return NotFound();
+        var cliente1 = _mapper.Map<Cliente>(clienteDto);
+        _unitOfWork.Clientes.Update(cliente1);
+        await _unitOfWork.SaveAsync();
+        return clienteDto;
+    }
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var cliente = await _unitOfWork.Clientes.GetByIdInt(id);
+        if (cliente == null)
+            return NotFound();
 
-            _unitOfWork.Clientes.Remove(cliente);
-            await _unitOfWork.SaveAsync();
+        _unitOfWork.Clientes.Remove(cliente);
+        await _unitOfWork.SaveAsync();
 
-            return NoContent();
-        }
+        return NoContent();
+    }
 }
